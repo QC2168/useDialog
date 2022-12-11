d607132535373b9e46972cb702f5f6f4d66d1d1a
山田 <huangxinhong2125@gmail.com>
2022-12-11 14:09:13 +0800

    update app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <template>
   <div class="app">
-    <el-button @click="openDialog1">打开DialogCmp (禁止全屏)</el-button>
-    <el-button @click="openDialog2">打开DialogCmp2 (可拖拽&可全屏)</el-button>
-    <el-button @click="openDialog3"
-      >打开DialogCmp3 (全屏&自定义页脚插槽)</el-button
-    >
-    <el-button @click="openDialog4(MODE.ADD)">表单弹窗（新增）</el-button>
-    <el-button @click="openDialog4(MODE.EDIT)">表单弹窗（编辑）</el-button>
+    <el-space wrap>
+      <el-button @click="openDialog1">普通弹窗</el-button>
+      <el-button @click="openDialog2">可拖拽和可全屏的弹窗</el-button>
+      <el-button @click="openDialog3">全屏和自定义页脚插槽弹窗</el-button>
+      <el-button @click="openDialog4(MODE.ADD)">表单弹窗（新增）</el-button>
+      <el-button @click="openDialog4(MODE.EDIT)">表单弹窗（编辑）</el-button>
+    </el-space>
     <DialogCmp
-      ref="DialogCmpRef"
       title="DialogCmp1"
       :hiddenFullBtn="true"
       v-model="visible1"
@@ -18,7 +17,6 @@
       <h3>DialogCmp1</h3>
     </DialogCmp>
     <DialogCmp2
-      ref="DialogCmpRef2"
       draggable
       title="DialogCmp2"
       v-model="visible2"
@@ -28,7 +26,6 @@
       <h3>DialogCmp2</h3>
     </DialogCmp2>
     <DialogCmp3
-      ref="DialogCmpRef3"
       draggable
       title="DialogCmp3"
       v-model="visible3"
